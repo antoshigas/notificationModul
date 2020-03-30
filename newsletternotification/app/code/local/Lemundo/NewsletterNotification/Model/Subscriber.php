b2255466a821c67a2689206b9992e4bc41d025d5
antoshigas <30558666+antoshigas@users.noreply.github.com>
2020-03-31 00:09:51 +0200

    Update Subscriber.php

--- newsletternotification/app/code/local/Lemundo/NewsletterNotification/Model/Subscriber.php
+++ newsletternotification/app/code/local/Lemundo/NewsletterNotification/Model/Subscriber.php
@@ -4,28 +4,31 @@ class Lemundo_NewsletterNotification_Model_Subscriber extends Mage_Newsletter_Mo
         /**
      * Sends out confirmation email
      *
+     * @return Mage_Newsletter_Model_Subscriber
      */
     public function sendConfirmationRequestEmail()
     {
-        $this->allEmailsConfig('confirmation', self::XML_PATH_CONFIRM_EMAIL_TEMPLATE, self::XML_PATH_CONFIRM_EMAIL_IDENTITY);
+        return $this->allEmailsConfig('confirmation', self::XML_PATH_CONFIRM_EMAIL_TEMPLATE, self::XML_PATH_CONFIRM_EMAIL_IDENTITY);
     }
 
     /**
      * Sends out confirmation success email
      *
+     * @return Mage_Newsletter_Model_Subscriber
      */
     public function sendConfirmationSuccessEmail()
     {
-        $this->allEmailsConfig('success', self::XML_PATH_SUCCESS_EMAIL_TEMPLATE, self::XML_PATH_SUCCESS_EMAIL_IDENTITY);
+        return $this->allEmailsConfig('success', self::XML_PATH_SUCCESS_EMAIL_TEMPLATE, self::XML_PATH_SUCCESS_EMAIL_IDENTITY);
     }
 
     /**
      * Sends out unsubsciption email
      *
+     * @return Mage_Newsletter_Model_Subscriber
      */
     public function sendUnsubscriptionEmail()
     {
-        $this->allEmailsConfig('unsubscription', self::XML_PATH_UNSUBSCRIBE_EMAIL_TEMPLATE, self::XML_PATH_UNSUBSCRIBE_EMAIL_IDENTITY);
+        return $this->allEmailsConfig('unsubscription', self::XML_PATH_UNSUBSCRIBE_EMAIL_TEMPLATE, self::XML_PATH_UNSUBSCRIBE_EMAIL_IDENTITY);
     }
 
     /**
